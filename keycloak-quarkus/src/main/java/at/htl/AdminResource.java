4274f0b15410d03409a7cc383554a8d21b97c910
Tamara Melcher <t.melcher.htlleonding@gmail.com>
2022-10-04 10:34:31 +0200

    add resources

--- keycloak-quarkus/src/main/java/at/htl/AdminResource.java
+++ keycloak-quarkus/src/main/java/at/htl/AdminResource.java
@@ -1,16 +1,17 @@
 package at.htl;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
-
+@Path("/api/admin")
+public class AdminResource {
     @GET
+    @RolesAllowed("admin")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String admin() {
+        return "granted";
     }
 }
